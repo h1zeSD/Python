6827dea8879b23a883c19552889f03a75180cb38
Mihail Sinygovski <hizesd6@gmail.com>
2026-01-26 18:04:54 +0300

    i do homework 1/26/2026

--- acmp892.py
+++ acmp892.py
@@ -1,3 +1,11 @@
-print(123)
-
-#test
+month_num = int(input())
+if month_num <= 0:
+    print("Error")
+elif month_num <= 2 or month_num == 12 :
+    print("Winter")
+elif month_num <= 5:
+    print("Spring")
+elif month_num <= 8:
+    print("Summer")
+elif month_num <= 11:
+    print("Autumn")
